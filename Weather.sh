9014cf6a3b0d7fcaa443b5bdbbf6ece2acfad900
Zach-Iglu <90990166+Zach-Iglu@users.noreply.github.com>
2022-01-21 21:15:19 -0500

    Create Weather.sh

---
+++ Weather.sh
@@ -0,0 +1,21 @@
+source ../../../preset_updater.sh ## ALWAYS INCLUDE THIS
+preset_file_name=`basename "$0"`
+desc="INSERT DESCRIPTION HERE"
+cooldown="0" # cooldown in minutes
+initializePreset
+### Valid Functions Below ###
+# getPluginVal <key>
+# getSettingsVal <key>
+# getDeviceVal <ip> <key>
+# setDeviceVal <ip> <key> <value>
+# getEvent <ip> <key>
+#### BEGIN USER CODe ####
+
+
+
+
+
+
+
+#### END USER CODe ####
+finishPreset
